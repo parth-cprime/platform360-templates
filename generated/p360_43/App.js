5e6f87cc87e6cc0fa82808d17e717d918c4fd633
parth-cprime <parth.patel@cprime.com>
2025-04-04 16:25:37 -0500

    Generated code for P360-43 - App.js

--- generated/p360_43/App.js
+++ generated/p360_43/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { AuthProvider } from './AuthContext';
-import PrivateRoute from './PrivateRoute';
-import Login from './Login';
-import Dashboard from './Dashboard';
-import Feedback from './Feedback';
+import { AuthProvider } from './auth/AuthContext';
+import { PrivateRoute } from './auth/PrivateRoute';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Switch>
-          <Route path="/login" component={Login} />
           <PrivateRoute exact path="/" component={Dashboard} />
-          <PrivateRoute path="/feedback/:id" component={Feedback} />
+          <Route path="/login" component={Login} />
         </Switch>
       </Router>
     </AuthProvider>
