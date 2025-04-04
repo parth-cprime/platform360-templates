fe34406370eec2ec21d376b1ccf1018dc6df2627
parth-cprime <parth.patel@cprime.com>
2025-04-04 16:45:01 -0500

    Generated code for P360-43 - PrivateRoute.js

--- generated/p360_43/PrivateRoute.js
+++ generated/p360_43/PrivateRoute.js
@@ -1,25 +1,11 @@
-import React, { useContext } from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { AuthContext } from './AuthContext';
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
-export const PrivateRoute = ({ component: Component, ...rest }) => {
-  const { user } = useContext(AuthContext);
+const PrivateRoute = ({ children }) => {
+  const { user } = useAuth();
 
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        user ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: '/login',
-              state: { from: props.location },
-            }}
-          />
-        )
-      }
-    />
-  );
+  return user ? children : <Navigate to="/login" />;
 };
+
+export default PrivateRoute;
