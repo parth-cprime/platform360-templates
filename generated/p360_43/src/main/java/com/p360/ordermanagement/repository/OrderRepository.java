2ae6456c90b0d296ae3786ae1582d2e3cb3a9e91
parth-cprime <parth.patel@cprime.com>
2025-04-11 10:41:56 -0500

    Generated code for P360-43 - src/main/java/com/p360/ordermanagement/repository/OrderRepository.java

---
+++ generated/p360_43/src/main/java/com/p360/ordermanagement/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.p360.ordermanagement.repository;
+
+import com.p360.ordermanagement.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
