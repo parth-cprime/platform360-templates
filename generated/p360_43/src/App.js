431f65d1bada8c7d83f5d461d59c4cbea53d54ce
parth-cprime <parth.patel@cprime.com>
2025-04-11 10:38:57 -0500

    Generated code for P360-43 - src/App.js

---
+++ generated/p360_43/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/" component={Dashboard} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
