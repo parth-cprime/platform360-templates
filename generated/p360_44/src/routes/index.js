8eab52b2c22358a078636a19a8db781ab114b60f
parth-cprime <parth.patel@cprime.com>
2025-04-11 11:32:28 -0500

    Generated code for P360-44 - src/routes/index.js

---
+++ generated/p360_44/src/routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+// Import your routes here
+// const userRoutes = require('./user');
+// router.use('/user', userRoutes);
+
+module.exports = router;
