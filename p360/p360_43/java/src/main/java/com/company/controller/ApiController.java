a69dc0e3fa5dcff4f692dcb95107d3f2009e00e6
parth-cprime <parth.patel@cprime.com>
2025-04-15 16:21:23 -0500

    Delete p360/p360_43/java/src/main/java/com/company directory

--- p360/p360_43/java/src/main/java/com/company/controller/ApiController.java
+++
@@ -1,9 +0,0 @@
-@RestController
-@RequestMapping("/api")
-public class ApiController {
-    
-    @GetMapping("/protected")
-    public ResponseEntity<?> getProtectedData() {
-        return ResponseEntity.ok(new ApiResponse("Protected data accessed successfully"));
-    }
-}
