78ebe1c2cab03a5a0856be30e1479fe8dfe39652
parth-cprime <parth.patel@cprime.com>
2025-04-15 14:25:43 -0500

    Generated code for P360-43 - main/java/com/company/service/NotificationService.java

---
+++ p360/p360_43/main/java/com/company/service/NotificationService.java
@@ -0,0 +1,11 @@
+@Service
+public class NotificationService {
+
+    public void sendNotification(Order order) {
+        // Implementation goes here
+    }
+
+    public void acknowledgeNotification(Acknowledgement acknowledgement) {
+        // Implementation goes here
+    }
+}
