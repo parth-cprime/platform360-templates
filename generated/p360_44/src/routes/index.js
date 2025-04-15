b80361ad1848cd405f179a2010bb1b9f5adbb44b
parth-cprime <parth.patel@cprime.com>
2025-04-15 14:18:33 -0500

    Delete generated/p360_44 directory

--- generated/p360_44/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Import your routes here
-// const userRoutes = require('./user');
-// router.use('/user', userRoutes);
-
-module.exports = router;
