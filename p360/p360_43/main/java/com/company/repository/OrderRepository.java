e954aa556adf9be3be7d532c43088d1aa438366f
parth-cprime <parth.patel@cprime.com>
2025-04-15 14:44:29 -0500

    Delete p360/p360_43 directory

--- p360/p360_43/main/java/com/company/repository/OrderRepository.java
+++
@@ -1,2 +0,0 @@
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- p360/p360_43/main/java/com/company/service/OrderService.java
+++
@@ -1,8 +0,0 @@
-@Service
-public class OrderService {
-    
-    @Autowired
-    private OrderRepository orderRepository;
-
-    // create, update, delete order methods
-}
