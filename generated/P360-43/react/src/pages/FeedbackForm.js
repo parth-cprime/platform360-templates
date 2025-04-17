5905ac4acb94566a3a04dffc986184fe8b7ac1e0
parth-cprime <parth.patel@cprime.com>
2025-04-17 12:37:16 -0500

    Add src/pages/FeedbackForm.js for task P360-43

---
+++ generated/P360-43/react/src/pages/FeedbackForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Feedback form page where users can submit feedback
+const FeedbackForm = () => {
+  return (
+    <div>
+      {/* Implement feedback form here */}
+    </div>
+  );
+};
+
+export default FeedbackForm;
