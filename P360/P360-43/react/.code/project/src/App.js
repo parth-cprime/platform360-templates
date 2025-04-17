3e6b353e0a9eebdebdf6f9ab54fbe878d4f7e05d
parth-cprime <parth.patel@cprime.com>
2025-04-17 12:44:50 -0500

    Add project/src/App.js for task P360-43

---
+++ P360/P360-43/react/.code/project/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+// Main application component
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
