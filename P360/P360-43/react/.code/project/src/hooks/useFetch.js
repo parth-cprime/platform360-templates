f12d14f8879192e8bac747ee32c81ea0ca5fa88a
parth-cprime <parth.patel@cprime.com>
2025-04-17 12:44:48 -0500

    Add project/src/hooks/useFetch.js for task P360-43

---
+++ P360/P360-43/react/.code/project/src/hooks/useFetch.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import api from '../services/api';
+
+// Custom hook to fetch data
+const useFetch = (url, options) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await api(url, options);
+        setResponse(res.data);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return { response, error, isLoading };
+};
+
+export default useFetch;
