50511a2b00d77aee1ad821fe30d3eb7b326fa880
parth-cprime <parth.patel@cprime.com>
2025-04-17 12:56:10 -0500

    Add src/components/errorBoundary.js for task P360-43

---
+++ P360/P360-43/react/src/components/errorBoundary.js
@@ -0,0 +1,31 @@
+// This component catches JavaScript errors anywhere in their child component tree, logs them, and displays a fallback UI
+
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        // Log the error to an error reporting service
+        console.error(error, errorInfo);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            // Render fallback UI
+            return <h1>Something went wrong.</h1>;
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
