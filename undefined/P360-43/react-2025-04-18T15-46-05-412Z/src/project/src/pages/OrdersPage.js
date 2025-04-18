e9f514a1711df31fbce4072c0267af1661363b63
parth-cprime <parth.patel@cprime.com>
2025-04-18 10:46:15 -0500

    Update undefined/P360-43/react-2025-04-18T15-46-05-412Z/src/project/src/pages/OrdersPage.js

---
+++ undefined/P360-43/react-2025-04-18T15-46-05-412Z/src/project/src/pages/OrdersPage.js
@@ -0,0 +1,14 @@
+```javascript
+import React from 'react';
+import OrderList from '../components/OrderList';
+
+// A page to display orders
+const OrdersPage = () => (
+    <div>
+        <h1>Orders Page</h1>
+        <OrderList />
+    </div>
+);
+
+export default OrdersPage;
+```
