b3319cb17950ab3f2db706d3be5fbdeba8704821
parth-cprime <parth.patel@cprime.com>
2025-04-18 11:32:25 -0500

    Update P360/P360-43/java/2025-04-18T16-32-15-798Z/project/src/utils/ExceptionHandler.java

---
+++ P360/P360-43/java/2025-04-18T16-32-15-798Z/project/src/utils/ExceptionHandler.java
@@ -0,0 +1,14 @@
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+@ControllerAdvice
+public class ExceptionHandler {
+
+    @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "This should be application specific";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
