6185120dd02737e08f9ea47aecfe6da0468dd6c4
parth-cprime <parth.patel@cprime.com>
2025-04-18 11:32:21 -0500

    Update P360/P360-43/java/2025-04-18T16-32-15-798Z/project/src/components/OrderFeedback.java

---
+++ P360/P360-43/java/2025-04-18T16-32-15-798Z/project/src/components/OrderFeedback.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class OrderFeedback {
+    private String orderId;
+    private String feedbackText;
+    private Date feedbackTime;
+    private boolean isUrgent;
+  
+    // Getters and Setters for fields
+}
