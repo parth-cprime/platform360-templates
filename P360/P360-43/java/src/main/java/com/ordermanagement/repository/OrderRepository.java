814e492f95a632697ccd1bb71f2050f6d4ab59a9
parth-cprime <parth.patel@cprime.com>
2025-04-18 10:09:36 -0500

    Delete P360/P360-43/java/src directory

--- P360/P360-43/java/src/main/java/com/ordermanagement/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ordermanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ordermanagement.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
