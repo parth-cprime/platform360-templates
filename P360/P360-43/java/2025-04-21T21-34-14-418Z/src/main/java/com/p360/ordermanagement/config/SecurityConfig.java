ba3a598ef4db7fc846eeea2e87429961658d2a6e
parth-cprime <parth.patel@cprime.com>
2025-04-21 16:34:20 -0500

    Update P360/P360-43/java/2025-04-21T21-34-14-418Z/src/main/java/com/p360/ordermanagement/config/SecurityConfig.java

---
+++ P360/P360-43/java/2025-04-21T21-34-14-418Z/src/main/java/com/p360/ordermanagement/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+```java
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/api/public/**").permitAll()
+            .antMatchers("/api/private/**").authenticated()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .addFilter(new JwtAuthenticationFilter(authenticationManager()));
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+```
