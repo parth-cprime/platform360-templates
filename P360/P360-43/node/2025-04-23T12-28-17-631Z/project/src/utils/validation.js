5766a1357f4515dd13e4940b66b05f74b9386a0c
parth-cprime <parth.patel@cprime.com>
2025-04-23 07:28:26 -0500

    Update P360/P360-43/node/2025-04-23T12-28-17-631Z/project/src/utils/validation.js

---
+++ P360/P360-43/node/2025-04-23T12-28-17-631Z/project/src/utils/validation.js
@@ -0,0 +1,13 @@
+// Import required packages
+const Joi = require("joi");
+
+// Input validation schema
+const userSchema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    email: Joi.string().email().required()
+});
+
+module.exports = {
+    userSchema
+};
