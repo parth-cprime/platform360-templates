3249a7b9c49e6e858fdc5b230f6807dad4a5309f
parth-cprime <parth.patel@cprime.com>
2025-04-25 12:17:05 -0500

    Update P360/P360-43/react/2025-04-25T17-16-53-009Z/src/components/PrivateRoute.js

---
+++ P360/P360-43/react/2025-04-25T17-16-53-009Z/src/components/PrivateRoute.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { currentUser } = useAuth();
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        currentUser ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
