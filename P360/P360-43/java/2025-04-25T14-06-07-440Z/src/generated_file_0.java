595955fc6c1eb2c0158813d52d97881178f87104
parth-cprime <parth.patel@cprime.com>
2025-04-25 09:06:09 -0500

    Update P360/P360-43/java/2025-04-25T14-06-07-440Z/src/generated_file_0.java

---
+++ P360/P360-43/java/2025-04-25T14-06-07-440Z/src/generated_file_0.java
@@ -0,0 +1,24 @@
+src/
+|-- main/
+|   |-- java/
+|   |   |-- com/
+|   |   |   |-- yourcompany/
+|   |   |   |   |-- notification/
+|   |   |   |   |   |-- NotificationApplication.java
+|   |   |   |   |   |-- controller/
+|   |   |   |   |   |   |-- FeedbackController.java
+|   |   |   |   |   |-- service/
+|   |   |   |   |   |   |-- FeedbackService.java
+|   |   |   |   |   |   |-- NotificationService.java
+|   |   |   |   |   |-- model/
+|   |   |   |   |   |   |-- Feedback.java
+|   |   |   |   |   |-- repository/
+|   |   |   |   |   |   |-- FeedbackRepository.java
+|   |   |   |   |   |-- config/
+|   |   |   |   |   |   |-- SecurityConfig.java
+|   |   |   |   |   |   |-- JwtTokenProvider.java
+|   |   |   |   |   |-- exception/
+|   |   |   |   |   |   |-- GlobalExceptionHandler.java
+|   |-- resources/
+|   |   |-- application.properties
+|-- test/
