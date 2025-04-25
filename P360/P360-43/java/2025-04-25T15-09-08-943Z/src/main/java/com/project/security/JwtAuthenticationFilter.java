dbc6dba82ba4e3b4b319c38b15e204b3d673d428
parth-cprime <parth.patel@cprime.com>
2025-04-25 10:09:18 -0500

    Update P360/P360-43/java/2025-04-25T15-09-08-943Z/src/main/java/com/project/security/JwtAuthenticationFilter.java

---
+++ P360/P360-43/java/2025-04-25T15-09-08-943Z/src/main/java/com/project/security/JwtAuthenticationFilter.java
@@ -0,0 +1,28 @@
+package com.project.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends GenericFilterBean {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            Authentication auth = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
