f225f80b304ac26e855b64e06a2951d79982307d
parth-cprime <parth.patel@cprime.com>
2025-04-28 10:54:39 -0500

    Update P360/P360-43/java/2025-04-28T15-54-32-116Z/src/services/UserDetailsServiceImplementation.java

---
+++ P360/P360-43/java/2025-04-28T15-54-32-116Z/src/services/UserDetailsServiceImplementation.java
@@ -0,0 +1,25 @@
+package services;
+
+import models.User;
+import repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import utils.UserPrincipal;
+
+@Service
+public class UserDetailsServiceImplementation implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username : " + username));
+
+        return UserPrincipal.create(user);
+    }
+}
