97475192ce2b2cfa654cadb694180300ab4cf28c
parth-cprime <parth.patel@cprime.com>
2025-04-29 12:04:07 -0500

    Update P360/P360-44/react/2025-04-29T17-03-55-031Z/src/utils/errorHandler.js

---
+++ P360/P360-44/react/2025-04-29T17-03-55-031Z/src/utils/errorHandler.js
@@ -0,0 +1,14 @@
+export const handleError = (error) => {
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.error("Data:", error.response.data);
+    console.error("Status:", error.response.status);
+  } else if (error.request) {
+    // The request was made but no response was received
+    console.error("Request:", error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.error("Error:", error.message);
+  }
+};
