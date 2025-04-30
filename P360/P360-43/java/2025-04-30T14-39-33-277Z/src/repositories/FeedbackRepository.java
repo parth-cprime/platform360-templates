8128095991a3022a9fbcfc75ed6d9c7f98369016
parth-cprime <parth.patel@cprime.com>
2025-04-30 09:39:43 -0500

    Update P360/P360-43/java/2025-04-30T14-39-33-277Z/src/repositories/FeedbackRepository.java

---
+++ P360/P360-43/java/2025-04-30T14-39-33-277Z/src/repositories/FeedbackRepository.java
@@ -0,0 +1,10 @@
+package repositories;
+
+import models.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    // Custom query methods can be added here if needed
+}
