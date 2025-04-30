59974bf50f5e728dbf142b208a06c947e2dcb00e
parth-cprime <parth.patel@cprime.com>
2025-04-30 12:53:47 -0500

    Update P360/P360-43/react/2025-04-30T17-53-35-945Z/src/pages/DashboardPage.js

---
+++ P360/P360-43/react/2025-04-30T17-53-35-945Z/src/pages/DashboardPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// Import necessary components and styles
+
+const DashboardPage = () => {
+    return (
+        <div>
+            {/* Dashboard content */}
+            <h1>Dashboard</h1>
+        </div>
+    );
+};
+
+export default DashboardPage;
