21d5f9c2c3e972fb4e75fdb0b63ce2302bd998a4
parth-cprime <parth.patel@cprime.com>
2025-04-30 11:26:30 -0500

    Update P360/P360-43/react/2025-04-30T16-26-15-438Z/src/utils/logger.js

---
+++ P360/P360-43/react/2025-04-30T16-26-15-438Z/src/utils/logger.js
@@ -0,0 +1,6 @@
+// Simple logger utility for consistent logging
+export const logger = {
+    log: (message) => console.log(message),
+    error: (message) => console.error(message),
+    info: (message) => console.info(message),
+};
