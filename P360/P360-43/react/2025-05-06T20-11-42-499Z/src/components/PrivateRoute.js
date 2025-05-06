74632391abb82071a4ba0f5f69ebafc149e58fe0
parth-cprime <parth.patel@cprime.com>
2025-05-06 13:11:50 -0700

    Update P360/P360-43/react/2025-05-06T20-11-42-499Z/src/components/PrivateRoute.js

---
+++ P360/P360-43/react/2025-05-06T20-11-42-499Z/src/components/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+// A wrapper for <Route> that redirects to the login
+// screen if you're not yet authenticated.
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { currentUser } = useAuth();
+
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        currentUser ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+}
+
+export default PrivateRoute;
