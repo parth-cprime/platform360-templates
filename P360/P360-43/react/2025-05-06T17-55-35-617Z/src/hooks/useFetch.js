e88800869d9b1bd5e92ed6622eb36ff027433fcb
parth-cprime <parth.patel@cprime.com>
2025-05-06 10:55:45 -0700

    Update P360/P360-43/react/2025-05-06T17-55-35-617Z/src/hooks/useFetch.js

---
+++ P360/P360-43/react/2025-05-06T17-55-35-617Z/src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url, options) => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url, options);
+                const data = await response.json();
+                setData(data);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [url, options]);
+
+    return { data, error, loading };
+};
+
+export default useFetch;
