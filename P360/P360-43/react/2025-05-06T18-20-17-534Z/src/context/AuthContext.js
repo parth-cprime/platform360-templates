f13b6b1e0daf0fb6d274a74f4943804fe4cf8f01
parth-cprime <parth.patel@cprime.com>
2025-05-06 11:20:28 -0700

    Update P360/P360-43/react/2025-05-06T18-20-17-534Z/src/context/AuthContext.js

---
+++ P360/P360-43/react/2025-05-06T18-20-17-534Z/src/context/AuthContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useContext, useState } from 'react';
+import { authenticateUser } from '../services/AuthService';
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    const login = async (username, password) => {
+        const user = await authenticateUser(username, password);
+        setUser(user);
+    };
+
+    const logout = () => {
+        setUser(null);
+    };
+
+    const value = {
+        user,
+        login,
+        logout,
+    };
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
