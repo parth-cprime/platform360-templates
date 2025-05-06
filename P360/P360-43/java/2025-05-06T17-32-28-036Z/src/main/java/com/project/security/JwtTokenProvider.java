6c1608be08d045cd207b640654990b45e6a7a4b5
parth-cprime <parth.patel@cprime.com>
2025-05-06 10:32:40 -0700

    Update P360/P360-43/java/2025-05-06T17-32-28-036Z/src/main/java/com/project/security/JwtTokenProvider.java

---
+++ P360/P360-43/java/2025-05-06T17-32-28-036Z/src/main/java/com/project/security/JwtTokenProvider.java
@@ -0,0 +1,34 @@
+package com.project.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${app.jwtExpirationInMs}")
+    private long jwtExpirationInMs;
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", userDetails.getAuthorities());
+        
+        return Jwts.builder()
+            .setClaims(claims)
+            .setSubject(userDetails.getUsername())
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationInMs))
+            .signWith(SignatureAlgorithm.HS512, jwtSecret)
+            .compact();
+    }
+}
