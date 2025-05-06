e33e094ebe5a4e916a293c398fb4efc4834ce3cd
parth-cprime <parth.patel@cprime.com>
2025-05-05 20:19:05 -0500

    Update P360/P360-44/react/2025-05-06T01-18-55-956Z/src/pages/NotFoundPage.js

---
+++ P360/P360-44/react/2025-05-06T01-18-55-956Z/src/pages/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Not found (404) page component
+function NotFoundPage() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>We can't seem to find the page you're looking for.</p>
+    </div>
+  );
+}
+
+export default NotFoundPage;
