b25ee773eb31ebf9e11ce6c7c247a13396315328
parth-cprime <parth.patel@cprime.com>
2025-05-06 11:58:58 -0700

    Update P360/P360-43/java/2025-05-06T18-58-50-238Z/src/controllers/UserController.java

---
+++ P360/P360-43/java/2025-05-06T18-58-50-238Z/src/controllers/UserController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import dto.UserDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+
+@RestController
+public class UserController {
+    
+    @PostMapping("/users")
+    public ResponseEntity<?> createUser(@Valid @RequestBody UserDTO userDTO) {
+        // Logic to handle user creation
+        return ResponseEntity.ok().build();
+    }
+}
+
+File: src/dto/UserDTO.java
