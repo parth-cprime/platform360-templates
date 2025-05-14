fd1f142cc612d0c99dd8fa58f504774c36dd26bd
parth-cprime <parth.patel@cprime.com>
2025-05-14 09:59:37 -0500

    Update P360/P360-43/node/2025-05-14T14-59-23-382Z/src/utils/notificationSender.js

---
+++ P360/P360-43/node/2025-05-14T14-59-23-382Z/src/utils/notificationSender.js
@@ -0,0 +1,7 @@
+const sendNotification = async (feedback) => {
+    // Placeholder for sending notifications
+    // This could be an email, SMS, or internal messaging system
+    console.log('Sending notification for urgent feedback:', feedback);
+};
+
+module.exports = { sendNotification };
