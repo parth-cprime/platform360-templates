d461ee83d9e1a3329f5b0650eb96ae5dcea87770
parth-cprime <parth.patel@cprime.com>
2025-05-23 14:13:43 -0500

    Update P360/P360-44/java/2025-05-23T19-13-33-885Z/src/main/java/com/company/security/JwtTokenProvider.java

---
+++ P360/P360-44/java/2025-05-23T19-13-33-885Z/src/main/java/com/company/security/JwtTokenProvider.java
@@ -0,0 +1,56 @@
+package com.company.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtTokenProvider {
+    
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expirationInMs}")
+    private long jwtExpirationInMs;
+
+    public String generateToken(Authentication authentication) {
+        UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+
+        final Date createdDate = new Date();
+        final Date expirationDate = new Date(createdDate.getTime() + jwtExpirationInMs);
+
+        return Jwts.builder()
+            .setClaims(new HashMap<>())
+            .setSubject(userPrincipal.getUsername())
+            .setIssuedAt(createdDate)
+            .setExpiration(expirationDate)
+            .signWith(SignatureAlgorithm.HS512, jwtSecret)
+            .compact();
+    }
+
+    public Boolean validateToken(String token) {
+        return !isTokenExpired(token);
+    }
+
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = getClaimFromToken(token, Claims::getExpiration);
+        return expiration.before(new Date());
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+    }
+}
