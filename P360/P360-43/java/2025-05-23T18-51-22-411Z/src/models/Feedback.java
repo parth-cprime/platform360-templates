1b867f6686957964199bb58197d027d64c9586be
parth-cprime <parth.patel@cprime.com>
2025-05-23 13:51:34 -0500

    Update P360/P360-43/java/2025-05-23T18-51-22-411Z/src/models/Feedback.java

---
+++ P360/P360-43/java/2025-05-23T18-51-22-411Z/src/models/Feedback.java
@@ -0,0 +1,21 @@
+package models;
+
+public class Feedback {
+
+    private String content;
+
+    public Feedback() {
+    }
+
+    public Feedback(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
