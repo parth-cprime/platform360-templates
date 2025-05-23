c486ad1062c79add7319ad9108958cd02d5a5853
parth-cprime <parth.patel@cprime.com>
2025-05-23 14:12:34 -0500

    Update P360/P360-44/react/2025-05-23T19-12-23-459Z/src/pages/HomePage.js

---
+++ P360/P360-44/react/2025-05-23T19-12-23-459Z/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the HomePage</h1>
+      {/* Additional content and components related to the homepage would go here */}
+    </div>
+  );
+}
