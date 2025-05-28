7d4afb3ae49fb34a1852b6f1d0c1af7dc08e3407
parth-cprime <parth.patel@cprime.com>
2025-05-27 21:16:11 -0500

    Update P360/P360-44/java/2025-05-28T02-15-56-773Z/src/main/java/com/company/service/DataService.java

---
+++ P360/P360-44/java/2025-05-28T02-15-56-773Z/src/main/java/com/company/service/DataService.java
@@ -0,0 +1,19 @@
+package com.company.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataService {
+    
+    @Autowired
+    private EncryptionService encryptionService;
+
+    public String encryptData(String data) {
+        return encryptionService.encrypt(data);
+    }
+
+    public String decryptData(String encryptedData) {
+        return encryptionService.decrypt(encryptedData);
+    }
+}
