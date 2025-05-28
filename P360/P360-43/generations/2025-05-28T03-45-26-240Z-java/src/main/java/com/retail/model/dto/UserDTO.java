64a677971d0dc2dc72cce418cc19a138c2538c57
parth-cprime <parth.patel@cprime.com>
2025-05-27 22:45:35 -0500

    Update P360/P360-43/generations/2025-05-28T03-45-26-240Z-java/src/main/java/com/retail/model/dto/UserDTO.java

---
+++ P360/P360-43/generations/2025-05-28T03-45-26-240Z-java/src/main/java/com/retail/model/dto/UserDTO.java
@@ -0,0 +1,50 @@
+package com.company.model.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+/**
+ * Data Transfer Object for user information.
+ */
+public class UserDTO {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 30, message = "Username must be between 3 and 30 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Pattern(regexp = "^[a-zA-Z0-9]{3,30}$", message = "Password must be alphanumeric and between 3 to 30 characters")
+    private String password;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    // Getters and Setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
