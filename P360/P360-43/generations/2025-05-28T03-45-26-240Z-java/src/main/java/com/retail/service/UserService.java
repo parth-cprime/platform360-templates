d44721d26ef20dc9f2b02cc037759e120eddbcdd
parth-cprime <parth.patel@cprime.com>
2025-05-27 22:45:38 -0500

    Update P360/P360-43/generations/2025-05-28T03-45-26-240Z-java/src/main/java/com/retail/service/UserService.java

---
+++ P360/P360-43/generations/2025-05-28T03-45-26-240Z-java/src/main/java/com/retail/service/UserService.java
@@ -0,0 +1,37 @@
+package com.company.service;
+
+import com.company.model.User;
+import com.company.model.dto.UserDTO;
+import com.company.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service layer for managing users.
+ */
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    /**
+     * Create a new user in the database.
+     * @param userDTO Data Transfer Object containing user details.
+     * @return The created User entity.
+     */
+    public User createUser(UserDTO userDTO) {
+        User user = new User();
+        user.setUsername(userDTO.getUsername());
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        user.setEmail(userDTO.getEmail());
+        return userRepository.save(user);
+    }
+}
