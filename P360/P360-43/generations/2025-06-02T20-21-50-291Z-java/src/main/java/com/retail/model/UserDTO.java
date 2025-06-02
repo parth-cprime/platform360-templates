37c1afbcc3e21fcce5fd39252021b7c713092f2f
parth-cprime <parth.patel@cprime.com>
2025-06-02 15:22:08 -0500

    Update P360/P360-43/generations/2025-06-02T20-21-50-291Z-java/src/main/java/com/retail/model/UserDTO.java

---
+++ P360/P360-43/generations/2025-06-02T20-21-50-291Z-java/src/main/java/com/retail/model/UserDTO.java
@@ -0,0 +1,23 @@
+package com.company.model.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class UserDTO {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 30, message = "Username must be between 3 and 30 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Pattern(regexp = "^[a-zA-Z0-9]{3,30}$", message = "Password must be alphanumeric and between 3 and 30 characters")
+    private String password;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    // Getters and Setters
+}
