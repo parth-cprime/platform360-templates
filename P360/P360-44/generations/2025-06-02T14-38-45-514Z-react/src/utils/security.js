464bb372af1889b8019038e951a19857a34acc7f
parth-cprime <parth.patel@cprime.com>
2025-06-02 09:38:55 -0500

    Update P360/P360-44/generations/2025-06-02T14-38-45-514Z-react/src/utils/security.js

---
+++ P360/P360-44/generations/2025-06-02T14-38-45-514Z-react/src/utils/security.js
@@ -0,0 +1,26 @@
+import CryptoJS from 'crypto-js';
+
+const SECRET_KEY = process.env.REACT_APP_ENCRYPTION_KEY;
+
+export const encryptData = (data) => {
+    return CryptoJS.AES.encrypt(JSON.stringify(data), SECRET_KEY).toString();
+};
+
+export const decryptData = (encryptedData) => {
+    const bytes = CryptoJS.AES.decrypt(encryptedData, SECRET_KEY);
+    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+};
+
+export const validateToken = (token) => {
+    try {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(c => {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        return JSON.parse(jsonPayload);
+    } catch (error) {
+        return null;
+    }
+};
